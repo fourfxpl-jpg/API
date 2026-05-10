5662851c8e7068d004d6bc5550c95ecb6a067688
fourfxpl-jpg <fourfxpl@gmail.com>
2026-05-10 14:39:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs      = require('fs');
 const path    = require('path');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const DB   = path.join(__dirname, 'keys.json');
 
 function loadDB() {
